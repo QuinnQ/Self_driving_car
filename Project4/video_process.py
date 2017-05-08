1a42740d1db8e99a32cd1d174af474af2094f5b2
Qian Wang <qian_wang@us.ibm.com>
2017-05-08 09:59:43 -0400

    Update video to show the curvature and car position

--- Project4/video_process.py
+++ Project4/video_process.py
@@ -2,7 +2,7 @@ from moviepy.editor import VideoFileClip
 # from IPython.display import HTML
 from pipeline import pipeline
 
-write_output = 'project_output_2.mp4'
+write_output = 'project_output.mp4'
 clip1 = VideoFileClip('project_video.mp4')
 write_clip = clip1.fl_image(pipeline)
 write_clip.write_videofile(write_output, audio=False)
